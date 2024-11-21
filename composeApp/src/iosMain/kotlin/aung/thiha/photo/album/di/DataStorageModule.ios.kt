73d28de52ea03f3652d3afce60d574f98c725183
Aung Thiha <mr.aungthiha@gmail.com>
2024-11-21 22:39:22 +0700

    refactor: remove redundant new line

--- composeApp/src/iosMain/kotlin/aung/thiha/photo/album/di/DataStorageModule.ios.kt
+++ composeApp/src/iosMain/kotlin/aung/thiha/photo/album/di/DataStorageModule.ios.kt
@@ -7,7 +7,6 @@ import aung.thiha.photo.album.storage.NAMED_AUTHENTICATION_PREFERENCE
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-
 actual val dataStorageModule: Module
     get() = module {
         single<DataStore<Preferences>>(NAMED_AUTHENTICATION_PREFERENCE) {
