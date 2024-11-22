b59bffbc7ae9376bb682ea418ecc082eabfa9d43
Aung Thiha <mr.aungthiha@gmail.com>
2024-11-22 16:48:41 +0700

    feat: setup Firebase for iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,14 @@
 import ComposeApp
 import SwiftUI
+import Firebase
 
 @main
 struct iOSApp: App {
+
+    init(){
+      FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
