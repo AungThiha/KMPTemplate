71dd7ee3551058461f6926c09ade598e5fa01412
Aung Thiha <mr.aungthiha@gmail.com>
2025-05-11 17:50:28 +0700

    refactor: move AuthenticationSession model into session:domain module

---
+++ storage/src/commonMain/kotlin/aung/thiha/storage/CreateKeyValueStorage.kt
@@ -0,0 +1,11 @@
+package aung.thiha.storage
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import okio.Path.Companion.toPath
+
+fun createKeyValueStorage(producePath: () -> String): DataStore<Preferences> =
+        PreferenceDataStoreFactory.createWithPath(
+            produceFile = { producePath().toPath() }
+        )
