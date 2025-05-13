ff8c0d481f8716910a7cbb4f3cc8a6f6f21ff49b
Aung Thiha <mr.aungthiha@gmail.com>
2025-05-14 01:21:45 +0700

    refactor: optimize imports in PhotoListScreen

--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/photos/presentation/PhotoListScreen.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/photos/presentation/PhotoListScreen.kt
@@ -3,15 +3,30 @@ package aung.thiha.photo.album.photos.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -55,6 +70,8 @@ fun PhotoListScreen(
             )
         }
     ) { contentPadding ->
+
+        LazyColumn {  }
         Box(modifier = Modifier.padding(contentPadding)) {
             when (photoListState) {
                 is PhotoListState.Content -> {
