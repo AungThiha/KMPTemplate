4d0d18439784b6db7cb6cac9961b70d1bf476ae7
Aung Thiha <mr.aungthiha@gmail.com>
2025-05-15 12:22:10 +0700

    chore: add TODO

--- design/src/commonMain/kotlin/aung/thiha/design/snackbar/SnackbarHostStateExt.kt
+++ design/src/commonMain/kotlin/aung/thiha/design/snackbar/SnackbarHostStateExt.kt
@@ -4,6 +4,9 @@ import androidx.compose.material.SnackbarHostState
 import org.jetbrains.compose.resources.getString
 
 suspend inline fun SnackbarHostState.showSnackbar(snackbarModel: SnackbarModel) {
+    // TODO instead of mapping it here, see if StringResource can be converted into String where context isn't available
+    // think about testing too. May be we don't need SnackbarMessage at all
+    // and we can just possibly do SnackbarModel(getString(stringResource)) right in SnackbarChannel class
     val message = when (val snackbarMessage = snackbarModel.message) {
         is SnackbarMessage.ResourceMessage -> getString(snackbarMessage.value)
         is SnackbarMessage.StringMessage -> snackbarMessage.value
