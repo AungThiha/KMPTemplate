b00355c70b1808c6aa15a4c16c6835e6621c2838
Aung Thiha <mr.aungthiha@gmail.com>
2025-06-03 21:17:08 +0700

    feat: use proper koin viewmodel injector

--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/di/SplashModule.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/di/SplashModule.kt
@@ -1,10 +1,11 @@
 package aung.thiha.photo.album.di
 
 import aung.thiha.photo.album.splash.SplashViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val splashModule = module {
-    factory {
+    viewModel {
         SplashViewModel(
             isSignedIn = get()
         )
--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/koin/KoinInit.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/koin/KoinInit.kt
@@ -3,6 +3,10 @@ package aung.thiha.photo.album.koin
 import aung.thiha.photo.album.di.appModule
 import org.koin.core.context.startKoin
 
+/**
+ * WARNING: This is used in [iOSApp.swift]
+ * Don't delete this
+* */
 fun initKoin(){
     startKoin {
         modules(appModule)
