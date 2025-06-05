f30a7198f506b2a9b595444c9ea1511ceef1763b
Aung Thiha <mr.aungthiha@gmail.com>
2025-06-05 13:43:27 +0700

    feat: move photos.data out into its own module

--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/di/PhotosModule.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/di/PhotosModule.kt
@@ -1,25 +1,12 @@
 package aung.thiha.photo.album.di
 
 import aung.thiha.photo.album.authentication.domain.usecase.Signout
-import aung.thiha.photo.album.photos.data.PhotosRepositoryImpl
-import aung.thiha.photo.album.photos.data.remote.service.PhotosDataSource
-import aung.thiha.photo.album.photos.data.remote.service.PhotosService
 import aung.thiha.photo.album.photos.domain.PhotosRepository
 import aung.thiha.photo.album.photos.presentation.PhotoListViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val photosModule = module {
-    single<PhotosDataSource> {
-        PhotosService(
-            httpClient = get()
-        )
-    }
-    factory<PhotosRepository> {
-        PhotosRepositoryImpl(
-            photosDataSource = get(),
-        )
-    }
     viewModel {
         PhotoListViewModel(
             _signout = get<Signout>(),
