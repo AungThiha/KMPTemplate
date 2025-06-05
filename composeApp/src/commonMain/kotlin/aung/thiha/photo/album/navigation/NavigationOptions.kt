d8a89a7e6f198ed53dd94c4c67c8ebe4223b863a
Aung Thiha <mr.aungthiha@gmail.com>
2025-06-05 11:37:32 +0700

    docs: design decision for navigation abstraction layer

--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/navigation/NavigationOptions.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/navigation/NavigationOptions.kt
@@ -3,6 +3,12 @@ package aung.thiha.photo.album.navigation
 import androidx.navigation.NavOptions
 import aung.thiha.compose.navigation.Destination
 
+/**
+ * The navigation abstraction layer is designed after to Jetpack Navigation so engineers can pick it up quickly.
+ * Then, why abstract at all? Mainly to make it easy to swap in another library if needed.
+ * When needed to swap in another library, might require a bit of adapter logic
+ * but the change will all be isolated inside the abstraction.
+ * */
 data class PopUpToOptions(
     val popUpToRoute: Destination? = null,
     val inclusive: Boolean = false
