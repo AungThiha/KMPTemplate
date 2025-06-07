c774b2d8d466357acc56c9cf6c3fbdf7c6801882
Aung Thiha <mr.aungthiha@gmail.com>
2025-06-07 15:49:27 +0700

    docs: mention the new koin API for instantiating ViewModel

--- composeApp/src/commonMain/kotlin/aung/thiha/photo/album/koin/GetViewModel.kt
+++ composeApp/src/commonMain/kotlin/aung/thiha/photo/album/koin/GetViewModel.kt
@@ -7,6 +7,11 @@ import org.koin.compose.getKoin
 
 @Composable
 inline fun <reified T : ViewModel> getViewModel() : T {
+    /**
+     * Latest koin version provides a better API for scoping ViewModel to LocalViewModelStoreOwner
+     * If you do not know what LocalViewModelStoreOwner is, don't worry about it for now
+     * You can come back to this once you have learned the basics
+     * */
     val koin = getKoin()
     return viewModel {
         koin.get<T>()
