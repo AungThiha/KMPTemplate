7f58e6d30ce93b8c7ba051d787b646b72c41d988
Aung Thiha <mr.aungthiha@gmail.com>
2025-06-21 22:24:36 +0700

    feat: remove firebase

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,14 @@
 import ComposeApp
 import SwiftUI
-import Firebase
+#if USE_FIREBASE
+import FirebaseCore
+#endif
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        #if USE_FIREBASE
         FirebaseApp.configure()
+        #endif
         KoinInitKt.doInitKoin()
         return true
     }
